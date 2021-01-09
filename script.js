d69042637b99bd09e5e88f6e42aa8ae507a2e406
raizy21 <chiper_raizy@yahoo.com>
2021-01-09 14:29:23 +0100

    init-spoke_roke_game

---
+++ script.js
@@ -0,0 +1,19 @@
+/******************************
+ *links
+ *
+ * {@link} - https://fontawesome.com/icons?d=gallery&q=close&m=free
+ * {@link} - https://fonts.google.com/
+ * {@link} - https://www.w3schools.com/js/js_switch.asp
+ * {@link} - https://www.youtube.com/watch?v=405Nh2H4Ucg
+ * {@link} - https://www.cssscript.com/confetti-falling-animation/
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+ * {@link} - https://www.sitepoint.com/understanding-es6-modules-via-their-history/
+ * {@link} - https://gist.github.com/branneman/558ef3a37ffd58ea004e00db5b201677
+ ******************************/
+
+
+ 
+
+
+
+
